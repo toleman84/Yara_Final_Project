5d360f440fde8d285a07d9e576dafcdb6c2a9515
Carlos Nadal <carlitosnadalfiuri05@gmail.com>
2025-05-08 16:53:46 -0300

    Changes to be committed: 	modified:   docker-compose.yml 	renamed:    loki/wal/00000257 -> loki/wal/00000276 	deleted:    loki/wal/checkpoint.000256/00000000 	renamed:    loki/wal/checkpoint.000255/00000000 -> loki/wal/checkpoint.000275/00000000 	modified:   postfix/entrypoint.sh 	modified:   promtail/config.yml

--- postfix/entrypoint.sh
+++ postfix/entrypoint.sh
@@ -2,7 +2,14 @@
 set -e
 
 # Start rsyslog daemon
+echo "[*] Starting rsyslogd..."
 rsyslogd
 
 # Start Postfix in foreground mode (container-friendly)
+echo "[*] Starting postfix in foreground mode...[*]"
 exec postfix start-fg
+echo "[*] postfix started...[*]"
+
+mkdir -p /var/log/scanner
+# This ensures all stdout/stderr (including the “Quarantined…” warnings) go into /var/log/scanner/scanner.log.
+exec python3 /app/scanner 2>&1 | tee -a /var/log/scanner/scanner.log
